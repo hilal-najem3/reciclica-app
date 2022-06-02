f42f8e875f080e7aeac585b1d2c448daed2e089e
hilal-najem3 <hilal123work@gmail.com>
2022-06-02 10:13:29 +0300

    Compplete loader page tests

--- src/app/pages/pickup-call/pickup-call.page.spec.ts
+++ src/app/pages/pickup-call/pickup-call.page.spec.ts
@@ -17,8 +17,4 @@ describe('PickupCallPage', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- src/app/pages/pickup-calls/pickup-calls.page.spec.ts
+++ src/app/pages/pickup-calls/pickup-calls.page.spec.ts
@@ -17,8 +17,4 @@ describe('PickupCallsPage', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
